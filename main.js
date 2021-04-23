aad99006644b6147f5a19e469f3c4313a8fe2384
ARay101 <81539939+ARay101@users.noreply.github.com>
2021-04-23 15:18:46 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function show() {
+    document.getElementById("lpecd").innerHTML = "the pizza is so delicious!";
+}
+function hide() { 
+    document.getElementById("lpecd").innerHTML = "la pizza è così deliziosa!";
+}
