8240d350f12f38c3721a395eddcc4e119f6d08a8
ARay101 <81539939+ARay101@users.noreply.github.com>
2021-04-26 11:05:29 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 function show() {
     document.getElementById("lpecd").innerHTML = "the pizza is so delicious!";
 }
-function hide() { 
+
+function hide() {
     document.getElementById("lpecd").innerHTML = "la pizza è così deliziosa!";
 }
